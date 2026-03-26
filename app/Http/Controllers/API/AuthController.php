1c046b6539325ea9891f41fb6e1729b050832472
Chitra Shrestha <ChitraShrestha12.com>
2026-03-26 20:40:43 +0545

    chore: add signup validation using Laravel Password rule with confirmed check

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
-    //
+    public function SignUp(Request $req)
+    {
+        $userValidate = Validator::make($req->all(), [
+            "full_name" => ['required'],
+            "email" => ['required', 'email', 'unique:users,email'],
+            "password" => ['required', Password::min(6)->mixedCase()->letters()->numbers()->symbols(), 'confirmed'],
+        ]);
+    }
 }
