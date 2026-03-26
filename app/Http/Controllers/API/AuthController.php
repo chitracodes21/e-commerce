9c5c85e18ca9bb6bbeec89c85235f82fc681caea
Chitra Shrestha <ChitraShrestha12.com>
2026-03-26 21:04:23 +0545

    feat: implement try-catch user creation for signup API

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
@@ -22,6 +23,24 @@ class AuthController extends Controller
                 'message' => 'Validation failed',
                 'errors' => $userValidate->errors()
             ], 422);
+        }
+        try {
+            $user = User::create([
+            "full_name" => $req->full_name,
+            "email" => $req->email,
+            "password" => $req->password,
+        ]);
+            return response()->json([
+                'success' => true,
+                'message' => 'User registered successfully',
+                'user' => $user
+            ], 201);
+        
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User registration failed'
+            ],500);
         }
     }
 }
